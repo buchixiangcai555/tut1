7576ced439a82435ac76b0084f4c7c6c3101201b
Iris <463717285@qq.com>
2021-09-09 11:11:53 +0800

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -17,10 +17,20 @@ public class Greeter {
   }
 
   public static void main(String[] args) {
-    String language = args[0];
-    String country = args[1];
-    String name = args[2];
-    Greeter greeter = new Greeter(language, country, name);
-    System.out.println(greeter.sayHello());
+    try {
+      if (args.length == 0) {
+        throw new IllegalArgumentException("No elements");
+      }
+      else {
+        String language = args[0];
+        String country = args[1];
+        String name = args[2];
+        Greeter greeter = new Greeter(language, country, name);
+        System.out.println(greeter.sayHello());
+      }
+    } catch (IllegalArgumentException e) {
+      System.err.println(e.getMessage());
+    }
+
   }
 }
